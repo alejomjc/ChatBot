53204e483f2dc5eda726d3b6387e20b02d7156f0
Miguel Alejandro Cepeda <37114980+alejomjc@users.noreply.github.com>
2024-12-26 03:49:54 -0500

    Refactor: Update startup event to use lifespan in FastAPI

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from .routers import user, chatbot, system
 from .database import init_db
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     init_db()
+    yield
 
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(user.router)
 app.include_router(chatbot.router)
